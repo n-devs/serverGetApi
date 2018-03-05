fb14a90b74b5b23190bad1545c98e308a082eeea
Naetirat <naetirat.s@gmail.com>
2018-03-05 09:56:27 +0700

    Update API 20180305

---
+++ querys/querys.js
@@ -0,0 +1,48 @@
+module.exports = {
+	functions: {
+		CURRENT_TIMESTAMP: {
+			toSqlString: function() {
+				return 'CURRENT_TIMESTAMP()';
+			}
+		}
+	},
+	users: {
+		isInTheDatabase: 'SELECT user_id FROM users WHERE email = ? AND password = ?',
+		create: 'INSERT INTO users (name, birthday, gender, phonenumber, email, password) VALUES (?, ?, ?, ?, ?, ?)',
+
+		getUserStoreList:
+			'SELECT uis.store_id, uis.user_status FROM users JOIN user_in_stores as uis ON users.user_id = uis.user_id WHERE users.user_id = ?'
+	},
+	stores: {
+		create:
+			'INSERT INTO stores (store_name, store_address, store_sub_district, store_district, store_province, store_postalcode) VALUES (?, ?, ?, ?, ?, ?)'
+	},
+
+	userInStores: {
+		create: 'INSERT INTO user_in_stores (user_id, store_id, user_status) VALUES (?, ?, ?)'
+	},
+
+	products: {
+		isInTheDatabase: 'SELECT count(*) as result FROM products WHERE product_barcode = ? AND product_brand = ?',
+
+		get:
+			'SELECT pd.product_id, pd.product_name, pd.product_brand, pis.product_price FROM products as pd JOIN product_in_stores as pis ON pd.product_id = pis.product_id WHERE pd.product_barcode = ? AND pis.store_id = ? ',
+
+		create:
+			'INSERT INTO products (product_barcode, product_name, product_brand, product_manufacturer, product_size, product_status) VALUES (?, ?, ?, ?, ?, ?)'
+	},
+
+	productInStores: {
+		isInTheDatabase: 'SELECT count(*) as result FROM product_in_stores WHERE product_id = ?AND store_id = ?',
+		create:
+			'INSERT INTO product_in_stores (product_id, store_id, product_quantity, product_price, product_minimum_quantity) VALUES (?, ?, ?, ?, ?)',
+
+		update:
+			'UPDATE product_in_stores SET product_quantity = product_quantity + ?, product_price = ?, product_minimum_quantity = ? WHERE product_id = ? AND store_id = ?'
+	},
+
+	productLog: {
+		create:
+			'INSERT INTO product_logs (product_id, store_id, product_log_date, product_log_quantity, product_log_cost, product_log_price) VALUES (?, ?, ?, ?, ?, ?)'
+	}
+};
--- routes/apicontrollers.js
+++ routes/apicontrollers.js
@@ -1,166 +1,270 @@
 var express = require('express');
 var mysql = require('mysql');
 var config = require('../config/database');
+var querys = require('../querys/querys');
 var router = express.Router();
 
-//test
-
-
-// Login
-router.post('/login', function (req, res) {
-  var email = req.body.email;
-  var password = req.body.password;
-
-  var connection = mysql.createConnection(config);
-
-  connection.query('select count(*) as result from users where email = ? and password = ?',
-    [email, password],
-    function (error, results, fields) {
-      if (error) throw res.send(error);
-      results[0].result === 1 ? res.json(1) : res.status(400).json(0);
-    });
-
-  connection.end();
-});
-
 // Register
-router.post('/register', function (req, res) {
-  var userData = {
-    email: req.body.email,
-    password: req.body.password
-  };
-  var connection = mysql.createConnection(config);
+router.post('/register', function(req, res) {
+	//TODO : Validatetion
+	var name = req.body.name;
+	var birthday = req.body.birthday;
+	var gender = req.body.gender;
+	var phonenumber = req.body.phonenumber;
+	var email = req.body.email;
+	var password = req.body.password;
 
-  connection.query('select count(*) as result from users where email = ? and password = ?',
-    [userData.email, userData.password],
-    function (error, results, fields) {
-      if (error) throw res.send(error);
-      if (!(results[0].result >= 1)) {
-        var connection = mysql.createConnection(config);
-        connection.query('insert into users (email, password) values (?, ?)',
-          [userData.email, userData.password],
-          function (error, results, fields) {
-            if (error) throw res.send(error);
-            res.send(`success user_id : ${results.insertId}`);
-          });
-      } else {
-        res.send('This users is already in our database');
-      }
-    });
+	var connection = mysql.createConnection(config);
 
+	connection.query(querys.users.isInTheDatabase, [ email, password ], function(error, results, fields) {
+		if (error) throw res.send(error);
+		if (!(results[0].result >= 1)) {
+			var connection = mysql.createConnection(config);
+			connection.query(querys.users.create, [ name, birthday, gender, phonenumber, email, password ], function(
+				error,
+				results,
+				fields
+			) {
+				if (error)
+					throw res.json({
+						status: 1,
+						error: error
+					});
+				res.json({
+					status: 1,
+					userId: results.insertId,
+					error: null
+				});
+			});
+		} else {
+			res.json({
+				status: 0,
+				error: 'This user is already in the database'
+			});
+		}
+	});
 
-  connection.end();
-
-});
-
-// Get all product from the database
-router.get('/product', (req, res) => {
-  var connection = mysql.createConnection(config);
-
-  connection.query('select * from products', (error, results, fields) => {
-    if (error) throw res.send(error);
-    results.length > 0 ? res.json(results) : res.json(0)
-  })
-  connection.end();
-})
-
-
-// Get specific product from the database
-router.get('/product/:productId', (req, res) => {
-  var productId = req.params.productId;
-  var connection = mysql.createConnection(config);
-
-  connection.query('select * from products where product_id = ?', productId, (error, results, fields) => {
-    if (error) throw res.send(error);
-    results.length > 0 ? res.json(results) : res.json(0);
-  });
-
-  connection.end();
-});
-
-// Get all product from the specific store
-router.get('/product/store/:storeId', (req, res) => {
-  var storeId = req.params.storeId;
-
-  var connection = mysql.createConnection(config);
-
-  connection.query('select * from products join product_in_stores on products.product_id = product_in_stores.product_id where store_id = ?',
-    storeId,
-    (error, results, fields) => {
-      if (error) throw res.send(error);
-      results.length > 0 ? res.json(results) : res.json(0);
-    });
-  connection.end();
-});
-
-// Get product from the specific store
-router.get('/product/:productId/store/:storeId', (req, res) => {
-  var storeId = req.params.storeId;
-  var productId = req.params.productId;
-  var connection = mysql.createConnection(config);
-
-  connection.query('select * from products join product_in_stores on products.product_id = product_in_stores.product_id where store_id = ? and products.product_id = ?',
-    [storeId, productId],
-    (error, results, fields) => {
-      if (error) throw res.send(error);
-      results.length > 0 ? res.json(results) : res.json(0);
-    });
-  connection.end();
-});
-
-
-// Add product to the database
-router.post('/product/add/:productName', (req, res) => {
-  var productName = req.params.productName;
-  var connection = mysql.createConnection(config);
-
-  connection.query('select count(*) as result from products where product_name = ?',
-    productName,
-    (error, results, fields) => {
-      if (error) throw res.send(error);
-      if (!(results[0].result >= 1)) {
-        var connection = mysql.createConnection(config);
-        connection.query('insert into products (product_name) values (?)',
-          productName,
-          (error, results, fields) => {
-            if (error) throw res.send(error);
-            res.send(`success product_id : ${results.insertId}`);
-          })
-      } else {
-        res.send(`${productName} is already in the database`);
-      }
-    })
-
-  connection.end();
+	connection.end();
 });
 
 // Add store to the database
-router.post('/store/add/:storeName', (req, res) => {
-  var storeName = req.params.storeName;
-  var connection = mysql.createConnection(config);
+router.post('/store', (req, res) => {
+	//TODO : Validation
+	var storeName = req.body.storeName;
+	var storeAddress = req.body.storeAddress;
+	var storeSubDistrict = req.body.storeSubDistrict;
+	var storeDistrict = req.body.storeDistrict;
+	var storeProvince = req.body.storeProvince;
+	var storePostalCode = req.body.storePostalCode;
+	var userId = req.body.userId;
+	var userStatus = req.body.userStatus;
 
-  connection.query('select count(*) as result from stores where store_name = ?',
-    storeName,
-    (error, results, fields) => {
-      if (error) throw res.send(error);
-      if (!(results[0].result >= 1)) {
-        var connection = mysql.createConnection(config);
-        connection.query('insert into stores (store_name) values (?)',
-          storeName,
-          (error, results, fields) => {
-            if (error) throw res.send(error);
-            res.send(`success store_id : ${results.insertId}`);
-          })
-      } else {
-        res.send(`${storeName} is already in the database`);
-      }
-    })
-})
+	var connection = mysql.createConnection(config);
+	connection.query(
+		querys.stores.create,
+		[ storeName, storeAddress, storeSubDistrict, storeDistrict, storeProvince, storePostalCode ],
+		(error, results, fields) => {
+			if (error)
+				throw res.json({
+					status: 0,
+					error: error
+				});
+			var storeId = results.insertId;
+			var connection = mysql.createConnection(config);
+			connection.query(querys.userInStores.create, [ userId, storeId, userStatus ], (error, results, fields) => {
+				if (error)
+					throw res.json({
+						status: 0,
+						error: error
+					});
+				res.json({
+					status: 1,
+					storeId: storeId,
+					error: null
+				});
+			});
+		}
+	);
+});
 
-router.post('', (req, res) => {
+// Add product to the databasse
+router.post('/product', (req, res) => {
+	// TODO : Validation
+	var productBarcode = req.body.productBarcode;
+	var productName = req.body.productName;
+	var productBrand = req.body.productBrand;
+	var productManufacturer = req.body.productManufacturer;
+	var productSize = req.body.productSize;
+	var productStatus = req.body.productStatus;
 
+	var connection = mysql.createConnection(config);
 
-})
+	// Check if product is in the database or not
+	connection.query(querys.products.isInTheDatabase, [ productBarcode, productBrand ], (error, results, fields) => {
+		if (error)
+			throw res.json({
+				status: 0,
+				error: error
+			});
+		if (!(results[0].result >= 1)) {
+			var connection = mysql.createConnection(config);
 
+			// Add product to products
+			connection.query(
+				querys.products.create,
+				[ productBarcode, productName, productBrand, productManufacturer, productSize, productStatus ],
+				(error, results, fields) => {
+					if (error)
+						throw res.json({
+							status: 0,
+							error: error
+						});
 
+					res.json({
+						status: 1,
+						productId: results.insertId,
+						error: null
+					});
+				}
+			);
+		} else
+			res.json({
+				status: 0,
+				error: 'This product is already in the database'
+			});
+	});
+});
+
+router
+	.route('/product/:productBarcodeOrId/store/:storeId')
+	// Get product from the database
+	.get((req, res) => {
+		var productBarcode = req.params.productBarcodeOrId;
+		var storeId = req.params.storeId;
+
+		var connection = mysql.createConnection(config);
+
+		connection.query(querys.products.get, [ productBarcode, storeId ], (error, results, fields) => {
+			if (error)
+				throw res.json({
+					status: 0,
+					error: error
+				});
+			res.json(results);
+		});
+	})
+	// Add product to store
+	.post((req, res) => {
+		//TODO : Validation
+		var productId = req.params.productBarcodeOrId;
+		var storeId = req.params.storeId;
+		var productCost = req.body.productCost;
+		var productQuantity = req.body.productQuantity;
+		var productPrice = req.body.productPrice;
+		var productMinimumQuantity = req.body.productMinimumQuantity;
+
+		var connection = mysql.createConnection(config);
+
+		connection.query(querys.productInStores.isInTheDatabase, [ productId, storeId ], (error, results, fields) => {
+			if (error)
+				throw res.json({
+					status: 0,
+					error: error
+				});
+			if (!(results[0].result >= 1)) {
+				var connection = mysql.createConnection(config);
+				connection.query(
+					querys.productInStores.create,
+					[ productId, storeId, productQuantity, productPrice, productMinimumQuantity ],
+					(error, results, fields) => {
+						if (error)
+							throw res.json({
+								status: 0,
+								error: error
+							});
+
+						res.json({
+							status: 1,
+							error: null
+						});
+					}
+				);
+			} else {
+				var connection = mysql.createConnection(config);
+				connection.query(
+					querys.productLog.create,
+					[
+						productId,
+						storeId,
+						querys.functions.CURRENT_TIMESTAMP,
+						productQuantity,
+						productCost,
+						productPrice
+					],
+					(error, results, fields) => {
+						if (error)
+							throw res.json({
+								status: 0,
+								error: error
+							});
+					}
+				);
+
+				connection.query(
+					querys.productInStores.update,
+					[ productQuantity, productPrice, productMinimumQuantity, productId, storeId ],
+					(error, results, fields) => {
+						if (error)
+							throw res.json({
+								status: 0,
+								error: error
+							});
+						res.json({
+							status: 1,
+							error: null
+						});
+					}
+				);
+			}
+		});
+	});
+
+//Login
+router.post('/login', (req, res) => {
+	var email = req.body.email;
+	var password = req.body.password;
+
+	var connection = mysql.createConnection(config);
+
+	connection.query(querys.users.isInTheDatabase, [ email, password ], (error, results, fields) => {
+		if (error)
+			res.json({
+				status: 0,
+				error: error
+			});
+
+		if (results.length === 1) {
+			var userId = results[0].user_id;
+
+			var connection = mysql.createConnection(config);
+			connection.query(querys.users.getUserStoreList, [ userId ], (error, results, fields) => {
+				if (error)
+					throw res.json({
+						status: 0,
+						error: error
+					});
+				res.json({
+					status: 1,
+					user_id: userId,
+					userStoreList: results,
+					error: null
+				});
+			});
+		} else
+			res.json({
+				status: 0,
+				error: 'wrong username or password'
+			});
+	});
+});
 module.exports = router;
