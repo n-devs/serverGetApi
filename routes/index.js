5ca8d308d0380ac576a9ee4b984b655f9af42208
Naetirat <naetirat.s@gmail.com>
2018-03-11 23:03:59 +0700

    Add productRegister form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+// TODO : find a way to get server ip address
+var serverAddress = 'http://150.95.26.138'
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
 });
-
+router.get('/product', function(req, res, next) {
+	res.render('product', {serverAddress : serverAddress});
+});
 module.exports = router;
