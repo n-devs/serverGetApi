6ec335c571e51fdddcbe0b6fa5e899a79a7139f4
Naetirat <army2102@hotmail.com>
2018-03-13 21:41:02 +0700

    Add feature Product page can receive res with AJAX now

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 // TODO : find a way to get server ip address
-var serverAddress = 'http://150.95.26.138'
-
+// var serverAddress = 'http://150.95.26.138';
+var serverAddress = 'http://localhost';
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 	res.render('index', { title: 'Express' });
 });
-router.get('/product', function(req, res, next) {
-	res.render('product', {serverAddress : serverAddress});
+router.get('/product', function (req, res, next) {
+	res.render('product', { serverAddress: serverAddress });
 });
 module.exports = router;
